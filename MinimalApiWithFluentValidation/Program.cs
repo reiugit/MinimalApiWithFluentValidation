3a4e2b2d04e05b7657c313e328fec090f9e8f649
rei <67372053+reiugit@users.noreply.github.com>
2024-08-09 19:11:52 +0200

    Added 'ForEvolve.FluentValidation' and 'SharpGrip.FluentValidation'

--- MinimalApiWithFluentValidation/Program.cs
+++ MinimalApiWithFluentValidation/Program.cs
@@ -1,24 +1,38 @@
 using FluentValidation;
 using MinimalApiWithFluentValidation.Dtos;
+using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddFluentValidationAutoValidation();
+builder.AddFluentValidationEndpointFilter();
 
 var app = builder.Build();
 
-app.MapPost("/api/products", (ProductRequest productRequest, IValidator<ProductRequest> validator) =>
+app.MapPost("/api/products-v1", (ProductRequest productRequest, IValidator<ProductRequest> validator) =>
 {
     var result = validator.Validate(productRequest);
 
     if (result.IsValid)
     {
-        var response = productRequest.ToResponse();
-
-        return Results.Created($"/api/products/{response.Id}", productRequest.ToResponse());
+        return Results.Created($"/api/products/1", productRequest.ToResponse());
     }
 
     return Results.ValidationProblem(result.ToDictionary());
 });
+
+app.MapPost("/api/products-v2", (ProductRequest productRequest, IValidator<ProductRequest> validator) =>
+{
+    return Results.Created($"/api/products/1", productRequest.ToResponse());
+})
+.AddFluentValidationFilter();
+
+
+app.MapPost("/api/products-v3", (ProductRequest productRequest, IValidator<ProductRequest> validator) =>
+{
+    return Results.Created($"/api/products/1", productRequest.ToResponse());
+})
+.AddFluentValidationAutoValidation();
 
 app.Run();
